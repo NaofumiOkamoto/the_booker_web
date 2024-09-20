49d810728c57b41b27eaa07e12927008ad7ddd20
NaofumiOkamoto <n.okamooo@gmail.com>
2024-09-20 12:40:58 +0900

    refactor

--- frontend/src/components/WatchListModal.tsx
+++ frontend/src/components/WatchListModal.tsx
@@ -15,6 +15,7 @@ interface Item {
 const WatchListModal: React.FC<{setModal: (b: boolean) => void}> = ({setModal}) => {
   const env = import.meta.env.VITE_ENV;
   const [books, setBooks] = useState<Item[]>([])
+  const [loading, setLoading] = useState(false)
   const { uid } = useAuth()
   const handleCancel = () => {
     setModal(false)
@@ -23,9 +24,11 @@ const WatchListModal: React.FC<{setModal: (b: boolean) => void}> = ({setModal}) 
   useEffect(() => {
     console.log('effect')
     const getWathcList = async () => {
+      setLoading(true)
       const res = await axios.get(`${
           env === 'development' ? 'http://localhost:5001' : ''
         }/api/get-watchlist?uid=${uid}`)
+      setLoading(false)
       console.log(res.data.watchlist)
       setBooks(res.data.watchlist)
     }
@@ -37,20 +40,23 @@ const WatchListModal: React.FC<{setModal: (b: boolean) => void}> = ({setModal}) 
       <div className='modal'>
         <h3>ウォッチリスト</h3>
         <button className="cancel-button" onClick={handleCancel}>閉じる</button>
-        <table>
-          {books?.map((book) => {
-            return (
-              <tbody>
-                <tr>
-                  <td className='tr-img'><img src={book.image_url} width="100px" /></td>
-                  <td className='tr-name'><a href={`https://www.ebay.com/itm/${book.item_number}`} target='_blankt'>{book.title}</a><br />({book.item_number})</td>
-                  <td className='tr-ope'><button>選択<br />（まだ）</button></td>
-                </tr>
-              </tbody>
-            )
-          })}
-
-        </table>
+        {loading ?
+          <p>取得中</p>
+          :
+          <table>
+            {books?.map((book) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td className='tr-img'><img src={book.image_url} width="100px" /></td>
+                    <td className='tr-name'><a href={`https://www.ebay.com/itm/${book.item_number}`} target='_blankt'>{book.title}</a><br />({book.item_number})</td>
+                    <td className='tr-ope'><button>選択<br />（まだ）</button></td>
+                  </tr>
+                </tbody>
+              )
+            })}
+          </table>
+        }
       </div>
     </div>
   );
